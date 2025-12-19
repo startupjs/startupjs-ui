7bdad15db341e626ac0167cfa02e6debeabdd82e
Pavel Zhukov <cray0000@gmail.com>
2025-12-19 16:25:21 +0200

    chore: add comment to the old FileInput component explaining the .expo.js extension

--- ui/components/forms/FileInput/index.js
+++ ui/components/forms/FileInput/index.js
@@ -1,1 +1,4 @@
+// the actual component code is intentionally moved out to input.js/input.expo.js
+// so that it can be platform-specific (startupjs adds a priority extension .expo.js
+// which will be loaded first by the bundler if the project is on Expo).
 export { default } from './input'
