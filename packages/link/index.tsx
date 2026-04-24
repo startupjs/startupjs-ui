3c107a1c7f90680fabd6ef53a174922735041e03
Your Name <you@example.com>
2026-04-24 10:05:02 +0300

    fix(Link): link handle mailto on web

--- packages/link/index.tsx
+++ packages/link/index.tsx
@@ -9,7 +9,7 @@ import Span from '@startupjs-ui/span'
 import './index.cssx.styl'
 
 const isWeb = Platform.OS === 'web'
-const EXTERNAL_LINK_REGEXP = /^(https?:\/\/|\/\/)/i
+const EXTERNAL_LINK_REGEXP = /^(https?:\/\/|\/\/|mailto:)/i
 
 export default observer(themed('Link', Link))
 
@@ -103,9 +103,24 @@ function Link ({
       }
 
       if (EXTERNAL_LINK_REGEXP.test(target ?? '')) {
-        isWeb
-          ? window.open(target, '_blank')
-          : Linking.openURL(target ?? '')
+        const url = target ?? ''
+        if (/^mailto:/i.test(url)) {
+          if (isWeb && typeof document !== 'undefined') {
+            const a = document.createElement('a')
+            a.href = url
+            document.body.appendChild(a)
+            a.click()
+            document.body.removeChild(a)
+          } else {
+            Linking.openURL(url)
+          }
+        } else {
+          if (isWeb) {
+            window.open(target, '_blank')
+          } else {
+            Linking.openURL(target ?? '')
+          }
+        }
       } else {
         let method
         if (push) method = routerPush
